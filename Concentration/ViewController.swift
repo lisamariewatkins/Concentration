05d4663f4b1acb978894eeaa41b7b001729f7fe8
lwatkins2 <lwatkins2@UALC02VN0YNHTDD.underarmour.com>
2018-03-07 18:13:02 -0600

    flip card functinality enhancements

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,19 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var white = true
     
-    @IBOutlet weak var button1: UIButton!
-    
-    @IBAction func flipCard(_ sender: Any) {
-        if white {
-            button1.backgroundColor = UIColor.blue
-            white = false
-        } else {
-            button1.backgroundColor = UIColor.white
-            white = true
+    var flipCount = 0 {
+        // runs everytime flipCount changes
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
         }
     }
     
+    var emojiChoices = ["👻", "🎃", "👻", "🎃"]
+    
+    @IBOutlet var cardButtons: [UIButton]!
+
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        // if optional is in set state...
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("card not found")
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
+    }
 }
 
